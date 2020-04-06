93a215b83c3b4c19aa021d2208a6b678355c8353
Maksim Karatkevich <m.karatkevich@akveo.com>
2020-04-06 15:13:05 +0300

    init commit

---
+++ src/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return <h1>React App</h1>;
+  }
+}
+
+export default App;
